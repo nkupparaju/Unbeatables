72b08b33ef5f229e10f81c08289303c049648346
Matthew Layman <matthewtelayman@gmail.com>
2019-10-28 21:54:17 -0400

    initial commit of shell project

---
+++ unbeatables-hangman/src/test/java/unbeatables/hangmanbackend/HangmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package unbeatables.hangmanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangmanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
